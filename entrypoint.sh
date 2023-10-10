bc28ebca8442717f5f075d670c70b924242452fd
Vitaliy Tymofeyev <vt@esurance.ch>
2023-10-11 00:38:28 +0300

    add NC_AUTH_JWT_SECRET

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 # Copy the value from OPTIONS_NC_DB to NC_DB
 export NC_DB=$OPTIONS_NC_DB
+export NC_AUTH_JWT_SECRET=$OPTIONS_NC_AUTH_JWT_SECRET
 
 # Execute the original entrypoint/command
 exec "/usr/src/appEntry/start.sh"
